d834b9793e220af7f7919fd07232dd182ff1a6fe
Naren <narendran1997@gmail.com>
2023-03-23 10:03:38 +0530

    Section 1 intro

---
+++ python_refresher/string_formattinng/stringf.py
@@ -0,0 +1,11 @@
+name = "bob"
+greeting = "Hello, bob"
+print(greeting)
+name = "newR"
+greeting = f"Hello, {name}"
+print(greeting)
+
+name = "cbc"
+newGreet = "Hello, {}"
+newGreetWithFormat = newGreet.format(name)
+print(newGreet, newGreetWithFormat)
---
+++ python_refresher/user_input/uinp.py
@@ -0,0 +1,7 @@
+# name = input("Enter your name")
+# print(name)
+size_input = input("how big is ur house(in sq ft)")
+size_input = int(size_input)
+sq_m = size_input/10.8
+print(sq_m)
+print(f"My house is {sq_m} big")
