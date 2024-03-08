b77fffe5647bbac5f998ae67c4d7eb3e9a0b295c
Naren <narendran1997@gmail.com>
2024-03-08 16:09:06 +0530

    Section 7: Upto 84th Lecture Video

--- flask-demo-app/app.py
+++ flask-demo-app/app.py
@@ -6,6 +6,7 @@ import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tags import blp as TagsBlueprint
 
 
 def create_app(db_url=None):
@@ -35,6 +36,7 @@ def create_app(db_url=None):
 
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(TagsBlueprint)
 
     return app
 
