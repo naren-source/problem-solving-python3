d9508bc7c621e43e8ff46a28e9f027d76751afc4
Naren <narendran1997@gmail.com>
2024-03-20 20:29:43 +0530

    Section 11: Until 115 Lecture Video

--- flask-demo-app/app.py
+++ flask-demo-app/app.py
@@ -7,6 +7,7 @@ from db import db
 import models
 from blocklist import BLOCKLIST
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
@@ -17,6 +18,7 @@ from resources.user import blp as UserBlueprint
 def create_app(db_url=None):
 
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTION"] = True
     app.config["API_TITLE"] = "Stores REST API"
