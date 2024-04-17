8fb82d38f250f3194458f26c8e379f0084f3d6eb
Naren <narendran.n@zs.com>
2024-04-17 17:33:06 +0530

    module 8 v3.1

--- problem_solving/latest/8_module/5_diagonalPrint.py
+++ problem_solving/latest/8_module/5_diagonalPrint.py
@@ -1,11 +1,23 @@
-# def generateDiagonal(matrixList: list, r: int, c: int) -> None:
-#     for rIdx in range(r):
-#         for cIdx in range(c):
-#
-#
-#
-# row, col = input().split()
-# matrix = [[None] * int(col)] * int(row)
-# for r in range(int(row)):
-#     matrix[r] = [int(c) for c in input().split()]
-# print(generateDiagonal(matrix, int(row), int(col)))
+def generateDiagonal(matrixList: list, r: int, c: int) -> None:
+    colShift = 0
+    rowShift = 0
+    while rowShift < r:
+        for cIdx in range(colShift, rowShift+colShift+1):
+            if cIdx < c:
+                print(matrixList[rowShift+colShift-cIdx][cIdx], end="       ")
+        print()
+
+        if colShift == (c-1):
+            break
+
+        if not (rowShift == r-1):
+            rowShift += 1
+        else:
+            colShift += 1
+
+
+row, col = input().split()
+matrix = [[None] * int(col)] * int(row)
+for x in range(int(row)):
+    matrix[x] = [int(c) for c in input().split()]
+generateDiagonal(matrix, int(row), int(col))
