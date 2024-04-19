13b0d5a9e758c4c870a8d9133f235fa11dea73ef
Naren <narendran.n@zs.com>
2024-04-19 16:54:24 +0530

    module 9 v3

--- problem_solving/70_remove_adj_dups.py
+++ problem_solving/70_remove_adj_dups.py
@@ -1,5 +1,37 @@
+def leftShift(lPos: int, rPos: int, inputStr: str) -> str:
+    strList = [*inputStr]
+    strResult = ""
+    for x in range(len(inputStr) - rPos):
+        strList[lPos + x] = strList[rPos + x]
+    return strResult.join(strList)
+
 def removeAdjacentDuplicates(iStr: str) -> str:
-    pass
+    left = 0
+    right = left + 1
+    loopTime = len(iStr)
+    strLen = loopTime
+    while True:
+        if iStr[left] == iStr[right]:
+            right += 1
+        else:
+            if right - left > 1:
+                iStr = leftShift(left, right, iStr)
+                loopTime = loopTime - (right-left)
+                left -= 1
+                right = left + 1
+            else:
+                left += 1
+                right += 1
+        if right == loopTime:
+            if loopTime == 1:
+                return "NILL"
+            if loopTime == strLen:
+                return iStr
+            break
+    resultStr = ""
+    for x in range(loopTime):
+        resultStr += iStr[x]
+    return resultStr
 
 
 print(removeAdjacentDuplicates(input()))
