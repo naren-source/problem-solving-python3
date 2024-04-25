ec5c3e360e22a1f0513dabf7341d83a0fcd1345a
Naren <imnaren12@gmail.com>
2024-04-25 19:08:26 +0530

    module 12 v3

--- problem_solving/100_max_sum_sub.py
+++ problem_solving/100_max_sum_sub.py
@@ -1,3 +1,40 @@
+def findMaxSumSub(nList: list, size: int) -> None:
+    left = 0
+    right = left
+    cSum = 0
+    mSum = [-1] * 4
+
+    while right < size:
+        if nList[right] < 0:
+            cSum = 0
+            left = right + 1
+            right = left
+        else:
+            cSum += nList[right]
+            if cSum >= mSum[0]:
+                if cSum == mSum[0]:
+                    if (right-left) > mSum[1]:
+                        mSum = cSum, right-left, left, right
+                else:
+                    mSum = cSum, right-left, left, right
+            right += 1
+
+    for x in range(mSum[2], mSum[3]+1):
+        if x == mSum[2]:
+            print('{', end="")
+            print(nList[x], end=",")
+        elif x == (mSum[3]):
+            print(nList[x], end="} ")
+        else:
+            print(nList[x], end=",")
+    print(mSum[0], end="")
+
+
+listSize = int(input())
+numList = [int(x) for x in input().split()]
+findMaxSumSub(numList, listSize)
+
+
 
 
 
